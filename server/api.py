c9aaca8316cd85a03026b329d9e667723f522463
Peter Bengtsson <mail@peterbe.com>
2013-08-07 13:40:58 -0700

    cleanup fixes

--- server/api.py
+++ server/api.py
@@ -50,6 +50,22 @@ boards = [
     }
 ]
 
+
+def cache_set(key, value, *args, **options):
+    if isinstance(value, (dict, list, bool)):
+        value = json.dumps(value)
+    cache.set(key, value, *args, **options)
+
+
+def cache_get(key, default=None):
+    value = cache.get(key)
+    if value is None:
+        value = default
+    if not isinstance(value, (dict, list, bool)):
+        value = json.loads(value)
+    return value
+
+
 class BoardView(MethodView):
 
     def post(self):
@@ -58,12 +74,10 @@ class BoardView(MethodView):
             abort(403)
             return
         token_cache_key = 'auth:%s' % token
-        user_info = cache.get(token_cache_key)
+        user_info = cache_get(token_cache_key)
         if not user_info:
             abort(400, "You have never logged in")
             return
-        else:
-            user_info = json.loads(user_info)
 
         name = request.json['name']
         components = request.json['components']
@@ -76,16 +90,12 @@ class BoardView(MethodView):
             'description': description,
             'creator': user_info['username'],
         }
-        cache.set(cache_key, json.dumps(data))  # infinite
+        cache_set(cache_key, data)  # indefinite
 
         boards_key = 'boards:%s' % token
-        previous = cache.get(boards_key)
-        if previous:
-            previous = json.loads(previous)
-        else:
-            previous = []
+        previous = cache_get(boards_key, [])
         previous.append(board_id)
-        cache.set(boards_key, json.dumps(previous))
+        cache_set(boards_key, previous)
 
         response = make_response(jsonify({'board': board_id}))
         return response
@@ -96,89 +106,67 @@ class BoardView(MethodView):
             return make_response(jsonify({'boards': []}))
 
         boards_key = 'boards:%s' % token
-        board_ids = cache.get(boards_key)
-        if board_ids:
-            board_ids = json.loads(board_ids)
-        else:
-            board_ids = []
+        board_ids = cache_get(boards_key, [])
         boards = []
         for board_id in board_ids:
             board_key = 'board:%s' % board_id
-            data = cache.get(board_key)
+            data = cache_get(board_key)
             if data:
-                data = json.loads(data)
                 boards.append(data)
         response = make_response(jsonify({'boards': boards}))
         return response
 
 
-app.add_url_rule('/api/board', view_func=BoardView.as_view('board'))
+class LogoutView(MethodView):
 
-
-
-@app.route('/api/logintest', methods=['GET'])
-def logintest():
-    cached_item = cache.get('test')
-    if cached_item is not None:
-        cache.inc('test')
-        return str(cached_item)
-    else:
-        cache.set('test', 0)
-        return 'okay then'
-
-
-@app.route('/api/logout', methods=['POST'])
-def logout():
-    cookie_token = str(request.cookies.get('token'))
-    response = make_response('logout')
-    response.set_cookie('token', '', expires=0)
-    response.set_cookie('username', '', expires=0)
-    # delete from memcache too
-    token_cache_key = 'token:%s' % cookie_token
-    cache.delete(token_cache_key)
-    return response
-
-
-@app.route('/api/login', methods=['POST'])
-def login():
-    login_payload = {
-        'Bugzilla_login': request.json['login'],
-        'Bugzilla_password': request.json['password'],
-        'Bugzilla_remember': 'on',
-        'GoAheadAndLogIn': 'Log in'
-    }
-    login_response = {}
-    r = requests.post(login_url, data=login_payload)
-    cookies = requests.utils.dict_from_cookiejar(r.cookies)
-    if cookies.has_key('Bugzilla_login'):
-        token = str(uuid.uuid4())
-        token_cache_key = 'auth:%s' % token
-        print "SETTING", token_cache_key
-        cache.set(token_cache_key, json.dumps({
-            'Bugzilla_login': cookies['Bugzilla_login'],
-            'Bugzilla_logincookie': cookies['Bugzilla_logincookie'],
-            'username': request.json['login']
-        }))
-        login_response['result'] = 'success'
-        login_response['token'] = token
-        response = make_response(jsonify(login_response))
-        response.set_cookie('token', token)
-        response.set_cookie('username', request.json['login'])
+    def post(self):
+        cookie_token = str(request.cookies.get('token'))
+        response = make_response('logout')
+        response.set_cookie('token', '', expires=0)
+        response.set_cookie('username', '', expires=0)
+        # delete from memcache too
+        token_cache_key = 'token:%s' % cookie_token
+        cache.delete(token_cache_key)
         return response
-    else:
-        abort(401)
-        print('login failed')
-        login_response['result'] = 'failed'
-        response = make_response(json.dumps(login_response))
-        return response
+
+
+class LoginView(MethodView):
+    def post(self):
+        login_payload = {
+            'Bugzilla_login': request.json['login'],
+            'Bugzilla_password': request.json['password'],
+            'Bugzilla_remember': 'on',
+            'GoAheadAndLogIn': 'Log in'
+        }
+        login_response = {}
+        r = requests.post(login_url, data=login_payload)
+        cookies = requests.utils.dict_from_cookiejar(r.cookies)
+        if 'Bugzilla_login' in cookies:
+            token = str(uuid.uuid4())
+            token_cache_key = 'auth:%s' % token
+            cache_set(token_cache_key, {
+                'Bugzilla_login': cookies['Bugzilla_login'],
+                'Bugzilla_logincookie': cookies['Bugzilla_logincookie'],
+                'username': request.json['login']
+            })
+            login_response['result'] = 'success'
+            login_response['token'] = token
+            response = make_response(jsonify(login_response))
+            response.set_cookie('token', token)
+            response.set_cookie('username', request.json['login'])
+            return response
+        else:
+            abort(401)
+            login_response['result'] = 'failed'
+            response = make_response(jsonify(login_response))
+            return response
 
 
 def augment_with_auth(request_arguments, token):
     if token is not None:
         user_cache_key = 'auth:%s' % token
-        user_info = cache.get(user_cache_key)
+        user_info = cache_get(user_cache_key)
         if user_info:
-            user_info = json.loads(user_info)
             request_arguments['userid'] = user_info['Bugzilla_login']
             request_arguments['cookie'] = user_info['Bugzilla_logincookie']
     return request_arguments
@@ -205,6 +193,11 @@ def catch_all(path):
     # key in the users dict then the response should remove all cookies so that
     # in the app users don't see they are logged in, if they aren't.
     return 'should be the index.html file, let angular handle the route - {0}'.format(path)
+
+
+app.add_url_rule('/api/board', view_func=BoardView.as_view('board'))
+app.add_url_rule('/api/logout', view_func=LogoutView.as_view('logout'))
+app.add_url_rule('/api/login', view_func=LoginView.as_view('login'))
 
 if __name__ == '__main__':
     app.debug = DEBUG
