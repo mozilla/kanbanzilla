3164c0418df8936859cdf6984bc716d536b4654a
Derek Ries <influenztial@gmail.com>
2013-08-07 16:25:59 -0700

    render boards with /board data

--- app/scripts/controllers/board.js
+++ app/scripts/controllers/board.js
@@ -3,60 +3,6 @@
 angular.module('kanbanzillaApp')
   .controller('BoardCtrl', ['$scope', '$location', '$q','Bugzilla', 'Boards', '$routeParams', '$window',
   function ($scope, $location, $q, Bugzilla, Boards, $routeParams, $window) {
-    var columns = ['UNCONFIRMED', 'NEW', 'ASSIGNED', 'RESOLVED'];
-
-    // for every component send off a request for all the bugs of varying statuses
-    // then we have to join the results for the same status and different component
-    function customLoader () {
-      var loadingPromises = [];
-      var board = Boards.get($routeParams.id);
-      if(board === undefined) {
-        $location.path('/');
-      }
-      else {
-        angular.forEach(board.components, function (component) {
-          angular.forEach(columns, function (column) {
-
-            var p = Bugzilla.getBugs({
-              product: component.product,
-              component: component.component,
-              status: column
-            });
-            p.success(function(data) {
-              var listName = column.toLowerCase() + 'Bugs';
-              $scope.loading[listName] = false;
-              $scope[listName].push.apply($scope[listName], data.bugs);
-            });
-
-            loadingPromises.push(p);
-
-          });
-        });
-        $q.all(loadingPromises).then(function (data) {
-          // when all requests are done
-        });
-      }
-    }
-
-    Boards.get($routeParams.id).
-      success(function (data) {
-        $scope.board = data;
-        console.log($scope.board);
-      });
-
-    function init () {
-      $scope.unconfirmedBugs = [];
-      $scope.resolvedBugs = [];
-      $scope.assignedBugs = [];
-      $scope.newBugs = [];
-      $scope.product = $routeParams.id;
-      $scope.loading = {
-        unconfirmedBugs: true,
-        resolvedBugs: true,
-        newBugs: true,
-        assignedBugs: true
-      };
-    }
     // ui-sortable options, placeholder is a class, and helper clone disables
     // the click event from firing when dropping cards.
     $scope.sortableOptions = {
@@ -65,8 +11,11 @@ angular.module('kanbanzillaApp')
       helper: 'clone'
     };
 
-    init();
-    // customLoader();
+    Boards.get($routeParams.id).
+      success(function (data) {
+        $scope.board = data;
+        console.log($scope.board);
+      });
 
     $scope.goToBugzilla = function (bug) {
       window.location = Bugzilla.getLink(bug.id);
@@ -78,7 +27,6 @@ angular.module('kanbanzillaApp')
 
     $scope.$on('$destroy', function () {
       console.log('board destroyed');
-      init();
     });
 
   }]);
