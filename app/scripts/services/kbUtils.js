54e14a31d8d9ea2a805ff80c4074c42ba0bb09ae
Derek Ries <influenztial@gmail.com>
2013-09-09 17:08:07 -0700

    formatting

--- app/scripts/services/kbUtils.js
+++ app/scripts/services/kbUtils.js
@@ -5,9 +5,9 @@ angular.module('kanbanzillaApp')
     // AngularJS will instantiate a singleton by calling "new" on this function
     return {
 
-      // takes a string of some duration of time and converts it to milliseconds
-      // ex:      0.5s -> 500     500ms -> 500     500 -> 500
       parseTimeExpression: function (timeExpression) {
+        // takes a string of some duration of time and converts it to milliseconds
+        // ex:      0.5s -> 500     500ms -> 500     500 -> 500
         var timeInMs,
             re = new RegExp(/[0-9]s/),
             s = timeExpression.search(re);
