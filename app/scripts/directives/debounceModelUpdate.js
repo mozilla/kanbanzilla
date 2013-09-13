6b8b176ad322dccec13923640237002af551825c
Derek Ries <influenztial@gmail.com>
2013-09-13 09:37:54 -0700

    cleaning up debounce directive

--- app/scripts/directives/debounceModelUpdate.js
+++ app/scripts/directives/debounceModelUpdate.js
@@ -1,7 +1,19 @@
 'use strict';
 angular.module('kanbanzillaApp')
   .directive('debounceModelUpdate', ['kbUtils', function(kbUtils) {
-
+    /**
+     * debounceModelUpdate is a directive that can be applied to input fields
+     * with the 'ng-model' directive. It will debounce the events that cause
+     * the ng-model to update. A common use case is for when you are applying
+     * a filter based on the model of an input to a large amount of data. Without
+     * debouncing, the model will update too frequently causing too many filters
+     * too happen and lag the application (specifically typing)
+     *
+     * Example Usage:
+     * <input type="text" ng-model="query" debounce-model-update="500ms">
+     *      or
+     * <input type="text" ng-model="query" debounce-model-update="0.5s">
+     */
       return {
         restrict: 'A',
         link: function( scope, element, attrs) {
@@ -11,35 +23,27 @@ angular.module('kanbanzillaApp')
           }
 
           var map = $._data(element[0], 'events');
-          console.log(map);
-          var map2 = angular.element(element[0]).data('events');
-          console.log(map2);
           var events = _.each(['input'], function (eventName, index) {
-            // only real dom events
-            if(eventName.charAt(0) !== '$') {
+            if(eventName.charAt(0) !== '$') { // only real dom events
               var handlerObj,
               debounced = _.debounce(function (event) {
                 for(var i = _.indexOf(map[eventName], debounceHandlerObj) + 1 ; i<map[eventName].length ; i++) {
                   handlerObj = map[eventName][i];
                   // call original event handler
                   handlerObj.handler.apply(this, arguments);
-                  // emulate regular event dispatching by
-                  // aborting further propagation when event
-                  // has state immediatePropagationStopped
+                  // emulate regular event propagation
+                  // dont propagate any further if it has immediatePropagationStopped
                   if(event.isImmediatePropagationStopped()) {
                     break;
                   }
                 }
               }, options.timeout);
-
               element.on( eventName, function (event) {
                 debounced.apply(this, arguments);
                 // tell jquery to suppress further propagation of this event
                 event.stopImmediatePropagation();
               });
-
-              // move our debounce handler at first position
-              // to be called before any other
+              // move our debounce handler to first position to make sure its called first
               var debounceHandlerObj = map.input.pop();
               map.input.unshift( debounceHandlerObj);
             }
